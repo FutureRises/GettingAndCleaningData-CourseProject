e6f90031349d7fa7883ce538577c6b4d961cd1d5
FutureRises <futurerises@gmail.com>
2019-12-25 01:57:20 -0500

    First submission of the assignment.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# GettingAndCleaningData-CourseProject
+---
+title: "CourseProject README file"
+author: "James Johnson"
+---
+
+
+#### Script leaves the following datasets in the environment:
+
+* finalData - The final dataset that was turned in containing the means of the variables.
+* allData - Dataset representing the merged training and testing data.
+* meanSTDSub - subset of the allData dataset with just the mean/std variables
+
+#### Additional Notes on script.
+
+* The code works as long as the 'UCI HAR Dataset' folder is in the working directory with the script.
+
+* The script requires the plyr and dplyr libraries and are loaded as part of the script.
+
+#### Execution
+
+1. Once libraries are loaded, the 'test' data and 'train' data are read into R separately. Extra spaces are removed and then the data is split along the remaining spaces.
+They are converted into a dataframes and their columns are converted into numerics.
+
+2. Features list is loaded into R. Numbers in the beginning and the parentheses are removed. The features are added a column names to the data set.
+
+3. Activities and Subjects are loaded in to R and attached to datasets as addidtional columns.
+
+4. Training and testing data sets are merged (allData)
+
+5. Activity Names are mapped to the 'Activities' column.
+
+6. Features that measure 'mean and 'std' are subsetted from the larger dataset. (meanSTDsUB)
+
+7. The subset is ordered by subject and activity and the mean of each variable is taken
+
+8. Excess varibles are cleaned up.
--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,9 @@
 ## 5. From the data set in step 4, creates a second, independent tidy data set 
 ##    with the average of each variable for each activity and each subject.
 
+library(plyr)
+library(dplyr)
+
 ## load data test data
 testData <- read.delim("./UCI HAR Dataset/test/X_test.txt", header = FALSE, 
                        strip.white = TRUE, stringsAsFactors = FALSE)
@@ -15,23 +18,71 @@ testData <- as.character(testData$V1)
 testData <- gsub("  "," ",testData)
 testData <- strsplit(testData, " ")
 testData <- as.data.frame(do.call(rbind, testData))
+testData <- mutate_if(testData, is.factor, ~ as.numeric(as.character(.x)))
 
 ## load data train data
-trainData <- read.delim("./UCI HAR Dataset/train/X_train.txt", header = FALSE, 
-                        strip.white = TRUE)
+trainData <- read.delim("./UCI HAR Dataset/train/X_train.txt", header = FALSE,
+                        strip.white = TRUE, stringsAsFactors = FALSE)
 trainData <- as.character(trainData$V1)
 trainData <- gsub("  "," ",trainData)
 trainData <- strsplit(trainData, " ")
 trainData <- as.data.frame(do.call(rbind, trainData))
+trainData <- mutate_if(trainData, is.factor, ~ as.numeric(as.character(.x)))
 
-#testSubjects <- read.delim("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
-#colnames(testSubjects) <- "Subjects"
+## Load the features list
+features <- read.delim("./UCI HAR Dataset/features.txt", header = FALSE,
+                       stringsAsFactors = FALSE)
+features <- features$V1
+features <- lapply(features, function(features) sub("^[[:digit:]]{,3} ", "",
+                                                    features))
+features <- lapply(features, function(features) gsub("\\(\\)", "",features))
 
-#trainSubjects <- read.delim("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
-#colnames(trainSubjects) <- "Subjects"
+## Add features to datasets
+colnames(trainData) <- features
+colnames(testData) <- features
 
-#Combine Data and Subject IDs, and clean up Subject objects
-#testData <- cbind(testSubjects, testData)
-#trainData <- cbind(trainSubjects, trainData)
-#rm(testSubjects)
-#rm(trainSubjects)
+#load Activities
+testActivities <- read.delim("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+colnames(testActivities) <- "Activity"
+
+trainActivities <- read.delim("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+colnames(trainActivities) <- "Activity"
+
+#load test Subjects
+testSubjects <- read.delim("./UCI HAR Dataset/test/subject_test.txt", header = FALSE)
+colnames(testSubjects) <- "Subjects"
+
+trainSubjects <- read.delim("./UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+colnames(trainSubjects) <- "Subjects"
+
+#Combine Data and activities
+testData <- cbind(testSubjects, testActivities, testData)
+trainData <- cbind(trainSubjects, trainActivities, trainData)
+
+#Combine data sets
+allData <- rbind(testData, trainData)
+
+## Convert Activity ID's
+allData$Activity <- mapvalues(allData$Activity, c(1:6), 
+                              c("WALKING", "WALKING UPSTAIRS", 
+                                "WALKING DOWNSTAIRS", "SITTING", 
+                                "STANDING", "LAYING"))
+
+#Extract Mean/STD subset
+meanSTDSub <- allData[, grepl("Subject|Activity|mean|Mean|std", names(allData))]
+
+#Group by Subject and Activity get mean
+finalData <- meanSTDSub %>% group_by(Subjects, Activity) %>% 
+        summarize_at(.vars = colnames(meanSTDSub[3:88]), .funs = mean)
+
+
+#Clean up
+rm(testActivities)
+rm(trainActivities)
+rm(testSubjects)
+rm(trainSubjects)
+rm(features)
+rm(trainData)
+rm(testData)
+detach("package:dplyr", unload = TRUE)
+detach("package:plyr", unload = TRUE)
